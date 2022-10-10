b9626d4d10f909963223792030fdaa090775049e
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-10 10:37:20 +0000

    user input url

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # coding=utf-8
 import socket
-print('The socket info / ip adres of www.w3schools.com is:''\n')
-print(socket.gethostbyname("www.w3schools.com",))
-print('\n')
-print('The socket info / ip adres of www.roadmap.sh is:''\n')
-print(socket.gethostbyname("www.roadmap.sh"))
+web = input("enter url: ")
+print('The socket info / ip adres of', web, 'is:''\n')
+print(socket.gethostbyname("www." + web,))
