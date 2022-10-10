b87458954bd06880555bd2ecc5dae0c5c3da0e31
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-10 11:20:40 +0000

    Add a port scan to the input url

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+#!/usr/bin/python
 # coding=utf-8
+
 import socket
+import nmap
 
 web = input("enter url: ")
 
 webadress = (socket.gethostbyname("www." + web,))
 
 print('\n' + webadress, '\n' )
+
+nm = nmap.PortScanner()
+print(nm.nmap_version())
+nm.scan(webadress, '1-1024', '-v')
+print(nm.scaninfo())
+print(nm.csv())
