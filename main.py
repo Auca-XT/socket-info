e7f547ddd8be6a1a88c6e4095a8de7e817edeb29
Jonathan <114219464+Auca-XT@users.noreply.github.com>
2022-10-10 10:25:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+import socket
+print('The socket info / ip adres of www.w3schools.com is:''\n')
+print(socket.gethostbyname("www.w3schools.com",))
+print('\n')
+print('The socket info / ip adres of www.roadmap.sh is:''\n')
+print(socket.gethostbyname("www.roadmap.sh"))
